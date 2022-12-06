f607c509854016e039ce53c069f78ea34bb1c761
MateusPitura <119008106+MateusPitura@users.noreply.github.com>
2022-12-06 14:41:38 -0300

    Create MediaNTermos.c

---
+++ MediaNTermos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(void){
+    setlocale(LC_ALL, "Portuguese");
+    float valor, soma=0;
+    int count=0;
+    printf("Digite um valor: ");
+    scanf("%f", &valor);
+    while(valor>0){
+        soma+=valor;
+        printf("Digite outro valor: ");
+        scanf("%f", &valor);
+        count++;
+    }
+    printf("A media e %.2f", soma/count);
+}
