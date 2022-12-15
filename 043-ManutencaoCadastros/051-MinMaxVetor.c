de292e7be79f1a583cd650abeb4fd8d97633d1de
MateusPitura <mateuspitura@gmail.com>
2022-12-15 16:28:06 -0300

    Adicionado a função comuta

--- 043-ManutencaoCadastros/051-MinMaxVetor.c
+++ 043-ManutencaoCadastros/051-MinMaxVetor.c
@@ -3,10 +3,11 @@
 
 #define N 25
 
-void ordena(float *array, int size);
+void ordena(float *array, int size); //0: crescente, 1: decrescente
 void exibe(float *array, int size);
 float sorteia(int min, int max);
 void fill(float *array, int size, int min, int max);
+void comuta(float *varA, float *varB);
 
 int main(){
     float vetor[N];
@@ -23,9 +24,7 @@ void ordena(float *array, int size){
     for(int i=0; i<size; i++){
         for(int j=i; j<size; j++){
             if(*(array+i)>*(array+j)){
-                *(array+i)+=*(array+j); //A=A+B (A: 8 e B: 3)
-                *(array+j)=*(array+i)-*(array+j); //B=A-B (B: 5 e A: 8)
-                *(array+i)-=*(array+j); //A=A-B (A: 3 e B: 5)
+                comuta(array+i, array+j);
             }
         }
     }
@@ -45,4 +44,10 @@ void fill(float *array, int size, int min, int max){
     for(int i=0; i<size; i++){
         *(array+i)=sorteia(min, max);
     }
+}
+
+void comuta(float *varA, float *varB){
+    *varA+=*varB; //A=A+B (ex.: A=8 e B=3)
+    *varB=*varA-*varB; //B=A-B (ex.: B=5 e A=8)
+    *varA-=*varB; //A=A-B (ex.: A=3 e B=5)
 }
