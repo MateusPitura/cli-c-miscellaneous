4504a2736036fa8dfdd4fd246e9a834a5b0a440e
MateusPitura <mateuspitura@gmail.com>
2022-12-15 17:01:17 -0300

    Add function extremos
    Adicionando a função "extremos", que atribui para variáveis passadas no parâmetro o maior e o menor valor de um array.

--- 051-MinMaxVetor.c
+++ 051-MinMaxVetor.c
@@ -1,23 +1,33 @@
+/*
+Escreva uma funcao que receba um array de inteiros V e os enderecos de duas variaveis
+inteiras, min e max, e armazene nessas variaveis o valor minimo e maximo do array.
+Escreva tambem uma funcao main que use essa funcao.
+*/
+
 #include <stdio.h>
 #include <time.h>
 
 #define N 25
 
-void ordena(float *array, int size, char ctrl); //0: crescente, 1: decrescente
+void ordena(float *array, int size, char ctrl); //'0': crescente, '1': decrescente
 void exibe(float *array, int size);
 float sorteia(int min, int max);
 void fill(float *array, int size, int min, int max);
 void comuta(float *varA, float *varB);
+void extremos(float *array, int size, float *max, float *min);
 
 int main(){
     float vetor[N];
+    float maior, menor;
     srand(time(NULL));
     fill(vetor, N, 5, 10);
-    exibe(vetor, N);
-    printf("\n");
-    ordena(vetor, N, '1');
-    exibe(vetor, N);
-    printf("\n");
+    //exibe(vetor, N);
+    //printf("\n");
+    //ordena(vetor, N, '1');
+    extremos(vetor, N, &maior, &menor);
+    //exibe(vetor, N);
+    //printf("\n");
+    printf("Maior: %-6.2f Menor: %-6.2f", maior, menor);
 }
 
 void ordena(float *array, int size, char ctrl){
@@ -50,4 +60,17 @@ void comuta(float *varA, float *varB){
     *varA+=*varB; //A=A+B (ex.: A=8 e B=3)
     *varB=*varA-*varB; //B=A-B (ex.: B=5 e A=8)
     *varA-=*varB; //A=A-B (ex.: A=3 e B=5)
+}
+
+void extremos(float *array, int size, float *max, float *min){
+    *min=*array;
+    *max=*array;
+    for(int i=1; i<size; i++){
+        if(*max<*(array+i)){
+            comuta(max, array+i);
+        }
+        else if(*min>*(array+i)){
+            comuta(min, array+i);
+        }
+    }
 }
