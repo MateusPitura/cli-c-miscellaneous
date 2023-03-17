d3678d07363aa1c3d1b4cbd57cfafc0e689e717a
MateusPitura <mateuspitura@gmail.com>
2023-03-17 15:17:34 -0300

    Create 054-Pilha.c

---
+++ 054-Pilha.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAMANHO 10
+
+char pilha[TAMANHO];
+
+void zeraVetor(int tamanho);
+char topo(void);
+int push(char input);
+int pop(void);
+void imprimi(void);
+
+int main(void){
+    zeraVetor(TAMANHO);
+    int escolha=0;
+    char c;
+    do{
+        system("cls");
+        printf("1 - Inserir\n2 - Remover\n3 - Pegar\n4 - Exibir\n5 - Sair\n\nEscolha: ");
+        scanf("%d", &escolha);
+        fflush(stdin);
+        switch (escolha){
+            case 1:
+                printf("Qual valor deseja inserir: ");
+                scanf("%c", &c);
+                fflush(stdin);
+                push(c);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                printf("\nVoce pegou: %c\n", topo());
+                system("pause");
+                break;
+            case 4:
+                imprimi();
+                break;
+            default:
+                printf("Digite um valor valido");
+                system("pause");
+        }
+    }while(escolha!=5);
+}
+
+void zeraVetor(int tamanho){
+    for(int i=0; i<tamanho; i++){
+        pilha[i]=0;
+    }
+}
+
+char topo(){
+    int i;
+    for(i=(TAMANHO-1); i>=0; i--){
+        if(pilha[i]!=0){
+            return pilha[i];
+        }
+    }
+    return i;
+}
+
+int push(char input){
+    int i;
+    for(i=0; i<TAMANHO; i++){
+        if(pilha[i]==0){
+            pilha[i]=input;
+            return 0;
+        }
+    }
+    return i;
+}
+
+int pop(){
+    int i=0;
+    for(i=(TAMANHO-1); i>=0; i--){
+        if(pilha[i]!=0){
+            pilha[i]=0;
+            return 0;
+        }
+    }
+    return i;
+}
+
+void imprimi(){
+    for(int i=(TAMANHO-1); i>=0; i--){
+        printf("| %1c |\n", pilha[i]);
+    }
+    system("pause");
+}
